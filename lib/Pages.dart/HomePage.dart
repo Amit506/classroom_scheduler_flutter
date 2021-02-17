c4bdca6ea47629ec800582647bb2db56077edbcc
Amit Kumar <amitkumar50608@gmail.com>
2021-02-17 11:35:57 +0530

    3 tabs added

--- lib/Pages.dart/HomePage.dart
+++ lib/Pages.dart/HomePage.dart
@@ -10,7 +10,7 @@ class HomePage extends StatelessWidget {
       length: 3,
           child: Scaffold(
         appBar: AppBar(
-
+  title: Text('Classroom Scheduler'),
           bottom: TabBar(
             tabs: [
               Text('Notices'),
