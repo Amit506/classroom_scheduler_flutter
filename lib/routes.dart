bf0b435a06c2e80c923f2265babc78f8bb392755
Amit Kumar <amitkumar50608@gmail.com>
2021-03-03 10:55:48 +0530

    updated

--- lib/routes.dart
+++ lib/routes.dart
@@ -5,11 +5,12 @@ import 'package:flutter/material.dart';
 import 'Pages.dart/HomePage.dart';
 import 'Pages.dart/LecturePage.dart';
 import 'Pages.dart/login.dart';
-
+import 'Pages.dart/PeoplePage.dart';
 final Map<String, WidgetBuilder> routes = {
   HomePage.routeName: (context) => HomePage(),
   LogInScreen.routeName: (context) => LogInScreen(),
   LogInScreen.routeName: (context) => LogInScreen(),
   LecturePage.routeName: (context) => LecturePage(),
   LandingPage.routename:(context)=> LandingPage(),
+
 };
