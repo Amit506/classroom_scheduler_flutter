8357e6706ebd3bd37040e183193962a9cf005252
88maybe <mayurika.borah@gmail.com>
2021-03-07 15:13:57 +0530

    People Page Counterpart

--- lib/Pages.dart/TabBars.dart/PeopleTabBar.dart
+++ lib/Pages.dart/TabBars.dart/PeopleTabBar.dart
@@ -1,11 +1,10 @@
+import 'package:classroom_scheduler_flutter/Pages.dart/Counterparts/PeopleCounterpart.dart';
+import 'package:classroom_scheduler_flutter/Pages.dart/PeoplePage.dart';
 import 'package:flutter/material.dart';
-
 
 class PeopleTabBar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
-    );
+    return PeoplePageCounterpart(); //PeoplePage();
   }
 }
