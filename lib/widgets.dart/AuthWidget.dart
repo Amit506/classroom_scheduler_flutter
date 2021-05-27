dcb25c81f1b1bb835c814b9ab96c5d44528cc5fe
unknown <amitkumar50608@gmail.com>
2021-05-27 15:14:31 +0530

    login resolved

--- lib/widgets.dart/AuthWidget.dart
+++ lib/widgets.dart/AuthWidget.dart
@@ -1,16 +1,10 @@
 import 'dart:async';
 
 import 'package:classroom_scheduler_flutter/Pages.dart/AuthenticationScreen.dart/LoginPage.dart';
-import 'package:classroom_scheduler_flutter/Pages.dart/Landing_page.dart/LandingPage.dart';
 import 'package:classroom_scheduler_flutter/Pages.dart/splashScreen.dart';
-import 'package:classroom_scheduler_flutter/Theme.dart/colors.dart';
 import 'package:classroom_scheduler_flutter/services/AuthService.dart';
 import 'package:classroom_scheduler_flutter/services/app_loger.dart';
-import 'package:classroom_scheduler_flutter/services/hub_root_data.dart';
-import 'package:classroom_scheduler_flutter/services/notification_manager.dart/firebase_notification.dart';
-import 'package:classroom_scheduler_flutter/services/notification_manager.dart/localnotification_manager.dart';
-import 'package:classroom_scheduler_flutter/services/notifications_new_installation.dart';
-import 'package:feature_discovery/feature_discovery.dart';
+
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
@@ -22,14 +16,11 @@ class AuthWidget extends StatelessWidget {
     return StreamBuilder<User>(
       stream: Provider.of<AuthService>(context).authState,
       builder: (_, snapshot) {
+        AppLogger.print(snapshot.data.toString());
         if (snapshot.hasData) {
-          if (snapshot.data == null) {
-            return LogInPage();
-          } else {
-            return SplashScreen();
-          }
+          return SplashScreen();
         } else {
-          return LoadingScreen();
+          return LogInPage();
         }
       },
     );
